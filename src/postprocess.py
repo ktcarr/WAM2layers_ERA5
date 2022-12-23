6393becc5e147bc161b0f38369839883c90d1e9a
Theo <kcarr@whoi.edu>
2022-12-23 13:14:20 -0500

    format with black

--- src/postprocess.py
+++ src/postprocess.py
@@ -3,6 +3,7 @@ import scipy.io as sio
 import datetime
 import os
 
+
 def data_path(y, doy_idx, years, timetracking):
     load_Sa_track = os.path.join(
         sub_interdata_folder, str(y) + "-" + str(doy_idx) + "Sa_track.mat"
@@ -52,7 +53,7 @@ if __name__ == "__main__":
     import argparse
 
     #### Read parameters #####
-    parser = argparse.ArgumentParser() 
+    parser = argparse.ArgumentParser()
 
     ## Specify year and days of year for tracking
     parser.add_argument("--year", dest="year", type=int)
@@ -65,7 +66,7 @@ if __name__ == "__main__":
     parser.add_argument("--lat_min", dest="lat_min", type=float, default=-90.0)
     parser.add_argument("--lat_max", dest="lat_max", type=float, default=90.0)
     parser.add_argument("--dlat", dest="dlat", type=float, default=1.0)
-    parser.add_argument("--dlon", dest="dlon", type=float, default=1.0) 
+    parser.add_argument("--dlon", dest="dlon", type=float, default=1.0)
 
     ## Data folders
     parser.add_argument("--fluxes_fp", dest="fluxes_fp", type=str)
@@ -85,14 +86,14 @@ if __name__ == "__main__":
     A_gridcell = constants["A_gridcell"]
     L_N_gridcell = constants["L_N_gridcell"]
     L_S_gridcell = constants["L_S_gridcell"]
-    L_EW_gridcell = constants["L_EW_gridcell"] 
+    L_EW_gridcell = constants["L_EW_gridcell"]
 
     doy_indices = utils.get_doy_indices(args.doy_start, args.doy_end, args.year)
 
     ##### Begin the actual postprocessing ####
-    start1 = timer() 
+    start1 = timer()
 
-    N = len(doy_indices) # number of days to process
+    N = len(doy_indices)  # number of days to process
 
     E_per_day = np.zeros((N, len(latitude), len(longitude)))
     E_track_per_day = np.zeros((N, len(latitude), len(longitude)))
@@ -105,18 +106,16 @@ if __name__ == "__main__":
     south_loss_per_day = np.zeros((N, 1, len(longitude)))
     down_to_top_per_day = np.zeros((N, len(latitude), len(longitude)))
     top_to_down_per_day = np.zeros((N, len(latitude), len(longitude)))
-    water_lost_per_day = np.zeros((N, len(latitude), len(longitude))) 
+    water_lost_per_day = np.zeros((N, len(latitude), len(longitude)))
 
     for i, doy_idx in enumerate(doy_indices):
         ## i is the loop iteration, doy_idx is the index of the DOY
 
         start = timer()
         datapath = data_path(y, doy_idx, years, timetracking)
-        
+
         # load tracked data
-        loading_ST = sio.loadmat(
-            datapath[0], verify_compressed_data_integrity=False
-        )
+        loading_ST = sio.loadmat(datapath[0], verify_compressed_data_integrity=False)
         Sa_track_top = loading_ST["Sa_track_top"]
         Sa_track_down = loading_ST["Sa_track_down"]
         north_loss = loading_ST["north_loss"]
@@ -125,11 +124,9 @@ if __name__ == "__main__":
         top_to_down = loading_ST["top_to_down"]
         water_lost = loading_ST["water_lost"]
         Sa_track = Sa_track_top + Sa_track_down
-        
+
         # load the total moisture data
-        loading_FS = sio.loadmat(
-            datapath[2], verify_compressed_data_integrity=False
-        )
+        loading_FS = sio.loadmat(datapath[2], verify_compressed_data_integrity=False)
         Fa_E_top = loading_FS["Fa_E_top"]
         Fa_N_top = loading_FS["Fa_N_top"]
         Fa_E_down = loading_FS["Fa_E_down"]
@@ -149,9 +146,7 @@ if __name__ == "__main__":
         E_per_day[i, :, :] = np.sum(E, axis=0)
         E_track_per_day[i, :, :] = np.sum(E_track, axis=0)
         P_per_day[i, :, :] = np.sum(P, axis=0)
-        Sa_track_down_per_day[i, :, :] = np.mean(
-            Sa_track_down[1:, :, :], axis=0
-        )
+        Sa_track_down_per_day[i, :, :] = np.mean(Sa_track_down[1:, :, :], axis=0)
         Sa_track_top_per_day[i, :, :] = np.mean(Sa_track_top[1:, :, :], axis=0)
         W_down_per_day[i, :, :] = np.mean(W_down[1:, :, :], axis=0)
         W_top_per_day[i, :, :] = np.mean(W_top[1:, :, :], axis=0)
@@ -161,7 +156,7 @@ if __name__ == "__main__":
         down_to_top_per_day[i, :, :] = np.sum(down_to_top, axis=0)
         top_to_down_per_day[i, :, :] = np.sum(top_to_down, axis=0)
         water_lost_per_day[i, :, :] = np.sum(water_lost, axis=0)
- 
+
         end = timer()
         print(
             "Runtime output for day " + str(doy_idx + 1) + " in year " + str(y) + " is",
@@ -182,14 +177,14 @@ if __name__ == "__main__":
                 "E_track_per_day": E_track_per_day,
                 "P_per_day": P_per_day,
                 "Sa_track_down_per_day": Sa_track_down_per_day,
-                "Sa_track_top_per_day": Sa_track_top_per_day, 
+                "Sa_track_top_per_day": Sa_track_top_per_day,
                 "W_down_per_day": W_down_per_day,
                 "W_top_per_day": W_top_per_day,
                 "E_time_per_day": E_time_per_day,
                 "water_lost_per_day": water_lost_per_day,
             },
             do_compression=True,
-        ) 
+        )
 
     end1 = timer()
     print("The total runtime of Con_E_Recyc_Output is", (end1 - start1), " seconds.")
